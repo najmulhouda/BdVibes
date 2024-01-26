31869f529b720fd94e3a5eae419989e05215e711
najmulhouda <najmulhouda6@gmail.com>
2024-01-26 19:21:52 +0600

    navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import SigninForm from "./_auth/forms/SigninForm";
+import SignupForm from "./_auth/forms/SignupForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./globals.css";
+
+const App = () => (
+  <main className="flex min-h-screen">
+    <Routes>
+      {/* Public route */}
+      <Route element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SigninForm />} />
+        <Route path="/sign-up" element={<SignupForm />} />
+      </Route>
+
+      {/* Private route */}
+      <Route element={<RootLayout />}>
+        <Route path="/" index element={<Home />} />
+      </Route>
+    </Routes>
+  </main>
+);
 
 export default App;
---
+++ src/_auth/forms/SignupForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Signup = () => {
+  return (
+    <div>
+      <Button>Signup</Button>
+    </div>
+  );
+};
+
+export default Signup;
