ddd695c930039ce720161f3dc6eb5d1062c56aad
najmulhouda <najmulhouda6@gmail.com>
2024-01-28 20:55:53 +0600

    environment setup completed

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -2,9 +2,13 @@ import { Account, Avatars, Client, Databases, Storage } from "appwrite";
 
 export const appwriteConfig = {
   projectId: import.meta.env.VITE_APPWRITE_PROJECT_ID,
+  url: import.meta.env.VITE_APPWRITE_URL,
 };
 
 export const client = new Client();
+
+client.setProject(appwriteConfig.projectId);
+client.setEndpoint(appwriteConfig.url);
 
 export const account = new Account(client);
 export const database = new Databases(client);
