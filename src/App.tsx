d27cea58fa6eb39d9dc87a7ea948ab3ba5141b55
najmulhouda <najmulhouda6@gmail.com>
2024-02-01 23:42:17 +0600

    router setup completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,17 @@ import AuthLayout from "./_auth/AuthLayout";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import RootLayout from "./_root/RootLayout";
-import { Home } from "./_root/pages";
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+  UpdateProfile,
+} from "./_root/pages";
 import "./globals.css";
 
 const App = () => (
@@ -18,7 +28,15 @@ const App = () => (
 
       {/* Private route */}
       <Route element={<RootLayout />}>
-        <Route path="/" index element={<Home />} />
+        <Route index element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/all-users" element={<AllUsers />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/update-post/:id" element={<EditPost />} />
+        <Route path="/posts/:id" element={<PostDetails />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/update-profile/:id" element={<UpdateProfile />} />
       </Route>
     </Routes>
     <Toaster />
