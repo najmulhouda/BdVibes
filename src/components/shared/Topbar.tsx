d22838ed041d081a086a1aac5c5de2c007cd7887
najmulhouda <najmulhouda6@gmail.com>
2024-02-01 23:15:50 +0600

    sidebar completed

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,7 +1,20 @@
-import { Link } from "react-router-dom";
+import { useUserContext } from "@/context/AuthContext";
+import { useSignOutAccount } from "@/lib/react-query/queriesAndMutation";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 
 const Topbar = () => {
+  const navigate = useNavigate();
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess]);
+
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
@@ -14,9 +27,20 @@ const Topbar = () => {
           />
         </Link>
         <div className="flex gap-4">
-          <Button variant="ghost" className="shad-button_ghost">
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={() => signOut()}
+          >
             <img src="/assets/icons/logout.svg" alt="logout" />
           </Button>
+          <Link to={`/profile${user.id}`} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              alt="profile"
+              className="h-8 w-8 rounded-full"
+            />
+          </Link>
         </div>
       </div>
     </section>
