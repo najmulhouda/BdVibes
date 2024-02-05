9797cdaa6866ac8f58ae899adb1f2e881e06eb74
najmulhouda <najmulhouda6@gmail.com>
2024-02-06 00:33:40 +0600

    datetime function created

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -19,6 +19,19 @@ const PostCard = ({ post }: PostCardProps) => {
               className="rounded-full w-12 lg:h-12"
             />
           </Link>
+          <div className="flex flex-col">
+            <p className="base-medium lg:body-bold text-light-1">
+              {post.creator.name}
+            </p>
+            <div className="flex-center gap-2 text-light-3">
+              <p className="subtle-semibold lg:small-regular">
+                {post.$createdAt}
+              </p>
+              <p className="subtle-semibold lg:small-regular">
+                {post.location}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
