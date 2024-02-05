0112d988da0092cdf16546df5deaa5fdaf2d23cc
najmulhouda <najmulhouda6@gmail.com>
2024-02-06 00:19:37 +0600

    postcard  card details

--- src/components/shared/PostCard.tsx
+++ src/components/shared/PostCard.tsx
@@ -1,5 +1,28 @@
-const PostCard = () => {
-  return <div>PostCard</div>;
+import { Models } from "appwrite";
+import { Link } from "react-router-dom";
+
+type PostCardProps = {
+  post: Models.Document;
+};
+const PostCard = ({ post }: PostCardProps) => {
+  return (
+    <div className="post-card">
+      <div className="flex-between">
+        <div className="flex items-center gap-3">
+          <Link to={`/profile/${post.creator.$id}`}>
+            <img
+              src={
+                post?.creator.imageUrl ||
+                "/assets/images/profile-placeholder.svg"
+              }
+              alt="creator"
+              className="rounded-full w-12 lg:h-12"
+            />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PostCard;
